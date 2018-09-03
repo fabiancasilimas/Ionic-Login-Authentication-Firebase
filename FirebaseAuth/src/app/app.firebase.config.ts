92c976ba9fe440a8426858bfda118dfc4c28ef67
Fabian Casilimas <fabiancasilimas@gmail.com>
2018-09-03 13:24:42 -0500

    Aplicación con Ionic, Firebase y AngularFire

---
+++ FirebaseAuth/src/app/app.firebase.config.ts
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = 
+{
+    apiKey: "AIzaSyB6k9nYMwAeOoEgzCAm3RIh8opM3ZoSAXg",
+    authDomain: "fir-auth-27f6c.firebaseapp.com",
+    databaseURL: "https://fir-auth-27f6c.firebaseio.com",
+    projectId: "fir-auth-27f6c",
+    storageBucket: "fir-auth-27f6c.appspot.com",
+    messagingSenderId: "93523278789"
+  };
---
+++ FirebaseAuth/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { LoginPage } from '../pages/login/login';
+import { TabsPage } from '../pages/tabs/tabs';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { FIREBASE_CONFIG } from './app.firebase.config';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    LoginPage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule
+
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    LoginPage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
